e8423abad6589b9981d528558f0be71daa20adf0
marta.padillaruiz@ucalgary.ca <fpadillaes@gmail.com>
2023-01-17 20:57:02 -0400

    EDR Unit class

---
+++ Pyxis/Core/OGC/EDR/Unit.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pyxis.Core.IO.EDR
+{
+    /// <summary>
+    /// A geometric feature.
+    /// </summary>
+    public class Unit : Object
+    {
+        /// <summary>
+        /// Gets or sets the label.
+        /// </summary>
+        [JsonProperty("label")]
+        public string Label { get; set; }
+
+        /// <summary>
+        /// Gets or sets the symbol.
+        /// </summary>
+        [JsonProperty("symbol")]
+        public Symbol Symbol { get; set; }
+
+    }
+
+    public class Symbol : Object
+    {
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        [JsonProperty("title", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        [JsonProperty("description", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        [JsonProperty("value", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Value { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type.
+        /// </summary>
+        [JsonProperty("type", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Type { get; set; }
+    }
+}
